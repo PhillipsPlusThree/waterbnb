ac9bfa5f0ad4a4665e50b7b6fd469811a2844935
Brooklyn <brooklyndparsons@gmail.com>
2023-06-20 15:36:50 -0500

    New Category

--- server/seed.sql
+++ server/seed.sql
@@ -1,14 +1,14 @@
-INSERT INTO rentals (location, price, date, group_size, image)
+INSERT INTO rentals (location, price, date, group_size, type, image)
 VALUES 
- ('Florence, Indiana', 177, '2023-06-18', 3, 'https://a0.muscache.com/im/pictures/9d6068de-9b64-4f5b-8eb5-f36c6256c9bb.jpg'),
- ('Providence, Rhode Island', 230, '2023-08-20', 2, 'https://a0.muscache.com/im/pictures/b8f03fa0-0115-4673-94b9-54808b9b93f1.jpg?im_w=1200'),
- ('Philadelphia, Pennsylvania', 390, '2023-06-22', 4, 'https://a0.muscache.com/im/pictures/miso/Hosting-32466576/original/ac05aaa2-1084-4c65-a6de-f1f7605709e6.png?im_w=960'),
- ('Racine, Wisconsin', 138, '2023-07-16', 3, 'https://a0.muscache.com/im/pictures/67f409bd-1edf-4bda-a12a-acdbb0ffd3de.jpg?im_w=960'),
- ('Racine, Wisconsin', 135, '2023-07-16', 4, 'https://a0.muscache.com/im/pictures/2418f484-b7d7-41c5-94b1-74a10a769f6d.jpg?im_w=960'),
- ('New Haven, Connecticut', 150, '2023-06-18', 7, 'https://a0.muscache.com/im/pictures/miso/Hosting-704968983424000708/original/78bdae6a-55cd-4c37-9eac-ae41ec8d60a8.jpeg?im_w=960'),
- ('Toms River, New Jersey', 250, '2023-07-09', 4, 'https://a0.muscache.com/im/pictures/miso/Hosting-715364694224479071/original/26762816-685f-4c86-8d4b-bdbc875d9f53.jpeg?im_w=960'),
- ('Brick Township, New Jersey', 165, '2023-06-25', 4, 'https://a0.muscache.com/im/pictures/3f2500bd-9207-456d-9fad-d60812c78233.jpg?im_w=960'),
- ('Annapolis, Maryland', 140, '2023-08-20', 4, 'https://a0.muscache.com/im/pictures/c5327c7f-6cde-4e5a-a57b-b63d4cfa2ce4.jpg?im_w=960'),
- ('Toronto, Canada', 128, '2023-06-25', 2, 'https://a0.muscache.com/im/pictures/miso/Hosting-616709482361818974/original/8ea339b7-c78b-407d-9970-0b22a449642b.jpeg?im_w=960');
+ ('Florence, Indiana', 177, '2023-06-18', 3, 'House Boats', 'https://a0.muscache.com/im/pictures/9d6068de-9b64-4f5b-8eb5-f36c6256c9bb.jpg'),
+ ('Providence, Rhode Island', 230, '2023-08-20', 2, 'House Boats', 'https://a0.muscache.com/im/pictures/b8f03fa0-0115-4673-94b9-54808b9b93f1.jpg?im_w=1200'),
+ ('Philadelphia, Pennsylvania', 390, '2023-06-22', 4, 'Yachts', 'https://a0.muscache.com/im/pictures/miso/Hosting-32466576/original/ac05aaa2-1084-4c65-a6de-f1f7605709e6.png?im_w=960'),
+ ('Racine, Wisconsin', 138, '2023-07-16', 3, 'Sail Boats', 'https://a0.muscache.com/im/pictures/67f409bd-1edf-4bda-a12a-acdbb0ffd3de.jpg?im_w=960'),
+ ('Racine, Wisconsin', 135, '2023-07-16', 4, 'House Boats', 'https://a0.muscache.com/im/pictures/2418f484-b7d7-41c5-94b1-74a10a769f6d.jpg?im_w=960'),
+ ('New Haven, Connecticut', 150, '2023-06-18', 7, 'House Boats', 'https://a0.muscache.com/im/pictures/miso/Hosting-704968983424000708/original/78bdae6a-55cd-4c37-9eac-ae41ec8d60a8.jpeg?im_w=960'),
+ ('Toms River, New Jersey', 250, '2023-07-09', 4, 'Sail Boats', 'https://a0.muscache.com/im/pictures/miso/Hosting-715364694224479071/original/26762816-685f-4c86-8d4b-bdbc875d9f53.jpeg?im_w=960'),
+ ('Brick Township, New Jersey', 165, '2023-06-25', 4, 'Yachts', 'https://a0.muscache.com/im/pictures/3f2500bd-9207-456d-9fad-d60812c78233.jpg?im_w=960'),
+ ('Annapolis, Maryland', 140, '2023-08-20', 4, 'Yachts', 'https://a0.muscache.com/im/pictures/2874874a-a6cb-4d03-a426-98fa4b0697c9.jpg?im_w=1200'),
+ ('Toronto, Canada', 128, '2023-06-25', 2, 'Yachts', 'https://a0.muscache.com/im/pictures/miso/Hosting-616709482361818974/original/8ea339b7-c78b-407d-9970-0b22a449642b.jpeg?im_w=960');
 
 
