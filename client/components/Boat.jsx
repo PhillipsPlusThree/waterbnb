19442b98f39fe06baa52c63d5f3d4076af474333
oscarmejia1776 <oscar.mejia17@gmail.com>
2023-06-22 13:45:02 -0400

    Reservation box, non-functional

--- client/components/Boat.jsx
+++ client/components/Boat.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { AiFillStar } from 'react-icons/ai';
+import { AiFillStar } from "react-icons/ai";
 import "../styles/boat.css";
+import Reservation from "./Reservation.jsx";
 
 const Boat = ({ rentalId }) => {
   const [rentalData, setRentalData] = useState(null);
@@ -25,25 +26,26 @@ const Boat = ({ rentalId }) => {
 
   return (
     <>
-    <div className="boat-des">
-      {/* <h2>${rentalData.name}</h2> */}
-      {/* <p>${rentalData.stars}${rentalData.location}</p> */}
-      <h2>{rentalData.description}</h2>
-      <div className="review">
-        <container className="rate">
-        <AiFillStar />
-      <p>{rentalData.rating}</p>
-      </container>
-      <p>{rentalData.review} reviews</p>
-      <p>{rentalData.location}</p>
+      <div className="boat-des">
+        {/* <h2>${rentalData.name}</h2> */}
+        {/* <p>${rentalData.stars}${rentalData.location}</p> */}
+        <h2>{rentalData.description}</h2>
+        <div className="review">
+          <container className="rate">
+            <AiFillStar />
+            <p>{rentalData.rating}</p>
+          </container>
+          <p>{rentalData.review} reviews</p>
+          <p>{rentalData.location}</p>
+        </div>
+        <img src={rentalData.image} alt="boat" />
+        <p>Entire boat hosted by Rebecca</p>
+        <p>{rentalData.group_size} guests</p>
+        {/* <p>${rentalData.description}</p> */}
+        <Reservation />
       </div>
-      <img src={rentalData.image} alt="boat" />
-      <p>Entire boat hosted by Rebecca</p>
-      <p>{rentalData.group_size} guests</p>
-      {/* <p>${rentalData.description}</p> */}
-    </div>
     </>
   );
-}; 
+};
 
 export default Boat;
